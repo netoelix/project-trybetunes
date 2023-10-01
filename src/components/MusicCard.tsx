16fed38ed9ea592bdb7bacfea9a8429c88be7880
Elix Neto <neto.elix@outlook.com>
2023-10-01 13:52:17 -0300

    Finalizando o requisito 5

--- src/components/MusicCard.tsx
+++ src/components/MusicCard.tsx
@@ -1,4 +1,11 @@
+import React, { useState } from 'react';
+
 function MusicCard({ song }) {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const toggleFavorite = () => {
+    setIsFavorite(!isFavorite);
+  };
   return (
     <div data-testid="music-card">
       <h3>{song.trackName}</h3>
@@ -9,6 +16,30 @@ function MusicCard({ song }) {
         <code>audio</code>
         .
       </audio>
+      <input
+        type="checkbox"
+        name=""
+        id={ `checkbox-music-${song.trackId}` }
+        checked={ isFavorite }
+        onChange={ toggleFavorite }
+      />
+      <label
+        htmlFor={ `checkbox-music-${song.trackId}` }
+        data-testid={ `checkbox-music-${song.trackId}` }
+      >
+        {isFavorite ? (
+          <img
+            src="/src/images/checked_heart.png"
+            alt="favorite"
+          />
+        ) : (
+          <img
+            src="/src/images/empty_heart.png"
+            alt="favorite"
+          />
+        )}
+        Adicionar aos favoritos
+      </label>
     </div>
   );
 }
